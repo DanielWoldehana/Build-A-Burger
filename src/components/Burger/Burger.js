5e49b0996f41db8721c99a06a74e78e8490311d2
Daniel Woldehana <danielwoldehana@MacBook-Air.local>
2021-02-16 15:27:36 -0500

    bug 1 fixed, burger is rendering

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,11 @@ import BurgerIngredient from "./BurgerIngredients/BurgerIngredients";
 
 const burger = (props) => {
   return (
-    <div className={classes.burger}>
+    <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
       <BurgerIngredient type="cheese" />
       <BurgerIngredient type="meat" />
       <BurgerIngredient type="bread-bottom" />
-      <p>This is working right?</p>
     </div>
   );
 };
